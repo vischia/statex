e9fbfc64fb042620d0b6a5cbf095c114ebc3ed94
Pietro Vischia <pietro.vischia@gmail.com>
2019-06-28 09:18:35 +0200

    Added getter for exercises

---
+++ pyunfold/get.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/jrbourbeau/pyunfold/master/docs/source/notebooks/tutorial.ipynb
+wget https://raw.githubusercontent.com/jrbourbeau/pyunfold/master/docs/source/notebooks/user_prior.ipynb
+wget https://raw.githubusercontent.com/jrbourbeau/pyunfold/master/docs/source/notebooks/regularization.ipynb
+wget https://raw.githubusercontent.com/jrbourbeau/pyunfold/master/docs/source/notebooks/multivariate.ipynb
+
